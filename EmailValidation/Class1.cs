5fbe1edf93832ad0b87df3463d77cd480938f036
piyush12346789 <piyushgupta024@gmail.com>
2020-10-06 10:39:12 +0530

    [Piyush] Add: Validating an email address.

---
+++ EmailValidation/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailValidation
+{
+    class Class1
+    {
+        string result = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+        public bool ValidateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, result);
+        }
+    }
+}
---
+++ EmailValidation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace EmailValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            bool result = class1.ValidateEmail("abc.123@yahoo.com.com");
+            Console.WriteLine(result);
+        }
+    }
+}
